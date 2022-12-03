191b33674f56fd5775d52be4b758cb298497aecf
GuillaumeDelore <deloreguillaume@gmail.com>
2022-12-03 14:27:06 +0400

    udpate of interval

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
 
-module.exports = nextConfig
+    return config;
+  },
+};
